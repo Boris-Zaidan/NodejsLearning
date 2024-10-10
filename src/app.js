c664363323aca0a706c2d2bfe07353f3c3a50080
Boris zaidan <Bzmgs31@gmail.com>
2024-10-10 13:51:10 -0300

    Update nas rotas e começando a fazer o crud

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-// Podemos fazer o mok
+// Podemos fazer o mock
 const client = [
   {
     id: 1,
@@ -23,17 +23,46 @@ const client = [
     age: 23,
     city: "juiz de fora-Mg",
   },
+  {
+    id: 4,
+    name: "Naiara",
+    age: 32,
+    city: "juiz de fora-Mg",
+  },
 ];
 
+function buscarClientPorId(id) {
+  return client.filter((client) => client.id == id);
+}
+
+//Pegar a posição ou index do elemento no array por id
+function burcarIndexClient(id) {
+  return client.findIndex((client) => client.id == id);
+}
+
 // Criar rota padrão ou raiz
-//http://localhost:3000/
 app.get("/", (req, res) => {
   res.status(200).send("Hello Word, Pequeno gafanhoto");
 });
 
-//http://localhost:3000/client
 app.get("/client", (req, res) => {
   res.status(200).send(client); // Enviando a lista dos clients
+});
+
+// Retorna o objeto por id
+app.get("/client/:id", (req, res) => {
+  res.json(buscarClientPorId(req.params.id));
+});
+
+app.post("/client", (req, res) => {
+  client.push(req.body);
+  res.status(201).send("Novo client cadastrado com sucesso!"); // Add novo client
+});
+
+app.delete("/client/:id", (req, res) => {
+  let index = burcarIndexClient(req.params.id);
+  client.splice(index, 1);
+  res.status(200).send(`Client com id ${req.params.id} excluido com sucesso`);
 });
 
 export default app;
