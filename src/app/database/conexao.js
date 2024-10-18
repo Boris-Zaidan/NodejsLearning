ec9a9f96164ed064441405fda42c7dc1d37d5658
Boris zaidan <Bzmgs31@gmail.com>
2024-10-18 16:54:13 -0300

    Refatoração seguindo o pdrão MVC

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -14,4 +14,23 @@ const conexao = mysql.createConnection({
 
 conexao.connect();
 
+/**
+ * Executa um código sql com ou sem valores
+ * @param {string} sql instrução sql a ser executada
+ * @param {string=id | [client, id]} valores a serem passados para o sql
+ * @param {string} msgReject mensagem a ser exibida
+ * @returns objeto da promise
+ */
+
+export const consulta = (sql, valores = "", msgReject) => {
+  return new Promise((resolve, reject) => {
+    conexao.query(sql, valores, (error, result) => {
+      if (error) {
+        return reject(msgReject);
+      }
+      resolve(result);
+    });
+  });
+};
+
 export default conexao;
--- src/app/repositories/ClientRepository.js
+++ src/app/repositories/ClientRepository.js
@@ -1,66 +1,31 @@
-import conexao from "../database/conexao.js";
+import { consulta } from "../database/conexao.js";
 
 class ClientRepository {
   //CRUD
 
   create(client) {
     const sql = "INSERT INTO clients SET ?";
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, client, (error, result) => {
-        if (error) {
-          return reject(`Erro ao criar novo client: ${error}`);
-        }
-        resolve(result);
-      });
-    });
+    return consulta(sql, client, "Não foi possível cadastar!");
   }
 
   findAll() {
     const sql = "SELECT * FROM clients;";
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, (error, result) => {
-        if (error) {
-          return reject(`Erro ao buscar os dados: ${error}`);
-        }
-        resolve(result);
-      });
-    });
+    return consulta(sql, "Não foi possível localizar");
   }
 
   findById(id) {
     const sql = "SELECT * FROM clients WHERE id=?;";
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, [id], (error, result) => {
-        if (error) {
-          return reject(`Erro ao buscar os dados: ${error}`);
-        }
-        resolve(result);
-      });
-    });
+    return consulta(sql, id, "Não foi possível localizar");
   }
 
   update(client, id) {
     const sql = "UPDATE clients SET ? WHERE id=?;";
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, [client, id], (error, result) => {
-        if (error) {
-          return reject(`Erro ao atualizar os dados: ${error}`);
-        }
-        resolve(result);
-      });
-    });
+    return consulta(sql, [client, id], "Não foi possível atualizar");
   }
 
   delete(id) {
     const sql = "DELETE FROM clients WHERE id=?;";
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, [id], (error, result) => {
-        if (error) {
-          return reject(`Erro ao deletar client: ${error}`);
-        }
-        resolve(result);
-      });
-    });
+    return consulta(sql, id, "Não foi possível deletar");
   }
 }
 
